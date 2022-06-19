f9bea4933e537adce779f8765c9e58f37677b983
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-06-19 20:38:16 -0230

    Added Javascript function for checking if classList.Toggle is active

--- practice/slidingmenus/js.js
+++ practice/slidingmenus/js.js
@@ -1,7 +1,12 @@
 function showMenu() {
+    
     document.getElementById('menu').classList.toggle('active');
 }
 
 function showTotals() {
     document.getElementById('totals').classList.toggle('active');
+}
+
+function isActive() {
+    
 }
