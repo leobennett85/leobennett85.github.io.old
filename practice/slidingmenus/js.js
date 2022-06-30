bad526b1fa9d442ac7577de7f8d0ac1a81a49fa3
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-06-30 16:33:14 -0230

    Setup HTML for Add a Row function

--- practice/slidingmenus/js.js
+++ practice/slidingmenus/js.js
@@ -1,3 +1,4 @@
+//Showing and hiding Rows
 function setMenus() {
     hideMenu();
     hideTotals();
@@ -44,3 +45,26 @@ function hideMenu() {
 function hideTotals() {
     document.getElementById('totals').classList.toggle('deactivateTotals');
 }
+/*
+//Adding Rows
+function 
+<div class="grid-wrapper">
+<div class="index">1</div>
+<div class="expcon" onclick="expand()">+</div>
+<div class="startingtime">2:00PM</div>
+<div class="starting">15 Mt. Pleasant Avenue, St. John's</div>
+<div class="ending">11 Fennelly's Rd, Portugal Cove</div>
+<div class="fare">$ 25.00</div>
+<div class="runningtotal">$ 25.00</div>
+<div id="content">
+  <div class="contentGridWrapper">
+    <div class="contotaltime">Total Time:</div>
+    <div class="condistance">Distance(km):</div>
+    <div class="confaretype">Type of Fare:</div>
+    <div class="condolperkm">$/km:</div>
+    <div class="confaretotal">Total Fare:</div>
+    <div class="contip">Tip:</div>
+  </div>
+</div>
+</div>
+*/
