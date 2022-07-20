27f62797defc14ec43f8bbdb7382809c664669f6
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-07-20 14:27:34 -0230

    Fixed logic of menu slides, updated functions to arrow functions

--- js/menu.js
+++ js/menu.js
@@ -1,42 +1,44 @@
-function showAdd() {
+showAdd = () => {
+    hideAll();
     let menu = document.getElementById('add');
     menu.classList.toggle('showAdd');
-
-    /*
-    let menu = document.getElementById('menu');
-    let totals = document.getElementById('totals');
-    
-    //menu is deactive - totals is active
-    if ((menu.classList.contains('hideAdd')) && (totals.classList.contains('activateTotals'))) {
-        totals.classList.replace('activateTotals', 'deactivateTotals');
-        menu.classList.replace('hideAdd', 'showAdd');
-    //menu is active - totals is deactive
-    } else if ((menu.classList.contains('showAdd')) && (totals.classList.contains('deactivateTotals'))) {
-        menu.classList.replace('showAdd', 'hideAdd');
-    //menu is deactive - totals is deactive
-    } else if ((menu.classList.contains('hideAdd')) && (totals.classList.contains('deactivateTotals'))) {
-        menu.classList.replace('hideAdd','showAdd');
-    }
-    */
 }
 
-function showUpdate() {
+showUpdate = () => {
+    hideAll();
     let menu = document.getElementById('update');
     menu.classList.toggle('showUpdate');
 }
 
-function showEdit() {
+showEdit = () => {
+    hideAll();
     let menu = document.getElementById('edit');
     menu.classList.toggle('showEdit');
 }
 
-function showEod() {
+showEod = () => {
+    hideAll();
     let menu = document.getElementById('eod');
     menu.classList.toggle('showEod');    
 }
 
-/* old showTotals */
-function showTotals() {
+showTotals = () => {
+    hideAll();
     let menu = document.getElementById('totals');
     menu.classList.toggle('showTotals');
 }
+
+hideAll = () => {
+    let add = document.getElementById('add');
+    let update = document.getElementById('update');
+    let edit = document.getElementById('edit');
+    let eod = document.getElementById('eod');
+    let totals = document.getElementById('totals');
+
+    add.classList.replace('showAdd', 'hideAdd');
+    update.classList.replace('showUpdate', 'hideUpdate');
+    edit.classList.replace('showEdit', 'hideEdit');
+    eod.classList.replace('showEod', 'hideEod');
+    totals.classList.replace('showTotals', 'hideTotals');
+
+};
