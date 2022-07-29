588ffc45850852f1672d3f7bd4b797120888cbeb
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-07-28 22:54:49 -0230

    Changed class to ids due to being unambigious

--- js/content.js
+++ js/content.js
@@ -15,41 +15,41 @@ contract = () => {
 
 //Create an object for the html of the page
 let newGridInfo = {
-    idGridIndfo: (index) => `<div id="gridInfo${index}">`,
-    idGridInfoLayout: (index) => `<div id="gridInfoLayout${index}">`,
+    idGridIndfo: `<div id="gridInfo">`,
+    idGridInfoLayout: `<div id="gridInfoLayout">`,
     clSpaceLeft: `<div class="space"></div>`,
-    clIndex: (index) => `<div class="infoStyle${index} index${index}">${index}</div>`,
-    clExpCon: (index) => `<div class="infoStyle${index} expcon${index}" onClick="expand();">+</div>`,
-    clTime: (index) => `<div class="infoStyle${index} time${index}"></div>`,
-    clStartAdd: (index) => `<div class="infoStyle${index} startadd${index}"></div>`,
-    clRunTotal: (index) => `<div class="infoStyle${index} runtotal${index}"></div>`,
+    clIndex: (index) => `<div id="${index}" class="infoStyle">${index}</div>`,
+    clExpCon: (index) => `<div id="expcon${index}" class="infoStyle" onClick="expand();">+</div>`,
+    clTime: (index) => `<div id="time${index}" class="infoStyle"></div>`,
+    clStartAdd: (index) => `<div id="startadd${index}" class="infoStyle"></div>`,
+    clRunTotal: (index) => `<div id=runtotal${index}" class="infoStyle"></div>`,
     clSpaceRight: `<div class="space"></div>`,
     closeDiv: `</div></div>`
 }
 
 let newGridDetails = {
-    idGridDetails: (index) => `<div id="gridDetails${index}">`,
-    idGridDetailsLayout: (index) => `<div id="gridDetailsLayout${index}">`,
+    idGridDetails: `<div id="gridDetails" class="gridDetailsStyle">`,
+    idGridDetailsLayout:`<div id="gridDetailsLayout">`,
     clSpaceA1: `<div class="space"></div>`,
     clSpaceA2: `<div class="space"></div>`,
     clSpaceA3: `<div class="space"></div>`,
-    clDetailsEndTime: (index) => `<div class="detailsStyle${index} detailsEndTime${index}">Arrival Time:</div>`,
-    clDetailsDestination: (index) => `<div class="detailsStyle${index} detailsDestination${index}">Destination Address:</div>`,
-    clDetailsFareTotal: (index) => `<div class="detailsStyle${index} detailsFareTotal${index}">Fare Total:</div>`,
+    clDetailsEndTime: (index) => `<div id= detailsEndTime${index}" class="detailsStyle">Arrival Time:</div>`,
+    clDetailsDestination: (index) => `<div id="detailsDestination${index}" class="detailsStyle">Destination Address:</div>`,
+    clDetailsFareTotal: (index) => `<div id="detailsFareTotal${index}" class="detailsStyle">Fare Total:</div>`,
     clSpaceB1: `<div class="space"></div>`,
     clSpaceB2: `<div class="space"></div>`,
     clSpaceB3: `<div class="space"></div>`,
     clSpaceB4: `<div class="space"></div>`,
-    clDetailsTotalTime: (index) => `<div class="detailsStyle${index} detailsTotalTime${index}">Total Time:</div>`,
-    clDetailsDistance: (index) => `<div class="detailsStyle${index} detailsDistance${index}">Distance (km):</div>`,
-    clDetailDollarsPerKm: (index) => `<div class="detailsStyle${index} detailsDollarsPerKm${index}">$/km:</div>`,
+    clDetailsTotalTime: (index) => `<div id="detailsTotalTime${index}" class="detailsStyle">Total Time:</div>`,
+    clDetailsDistance: (index) => `<div id="detailsDistance${index}" class="detailsStyle">Distance (km):</div>`,
+    clDetailDollarsPerKm: (index) => `<div id="detailsDollarsPerKm${index}" class="detailsStyle">$/km:</div>`,
     clSpaceC1: `<div class="space"></div>`,
     clSpaceC2: `<div class="space"></div>`,
     clSpaceC3: `<div class="space"></div>`,
     clSpaceC4: `<div class="space"></div>`,
     clSpaceC5: `<div class="space"></div>`,
-    clDetailsFareType: (index) => `<div class="detailsStyle${index} detailsFareType${index}">FareType:</div>`,
-    clDetailsTip: (index) => `<div class="detailsStyle${index} detailsTip${index}">Tip:</div>`,
+    clDetailsFareType: (index) => `<div id="detailsFareType${index}" class="detailsStyle">FareType:</div>`,
+    clDetailsTip: (index) => `<div id="detailsTip${index}" class="detailsStyle">Tip:</div>`,
     clspaceD1: `<div class="space"></div>`,
     closeDiv: `</div><div>`
 }
@@ -80,40 +80,44 @@ End of old Object */
 function addNewFareTable() {
     const fareTableElement = document.getElementById("fareTable");
     const fareTableCount = fareTableElement.childElementCount;
-    // gridInfo Section
-    newGridInfo.idGridIndfo(fareTableCount) +
-    newGridInfo.idGridInfoLayout(fareTableCount) +
-    newGridInfo.clIndex(fareTableCount) +
-    newGridInfo.clExpCon(fareTableCount) +
-    newGridInfo.clTime(fareTableCount) +
-    newGridInfo.clStartAdd(fareTableCount) +
-    newGridInfo.clRunTotal(fareTableCount) +
-    newGridInfo.clSpaceRight(fareTableCount) +
-    newGridInfo.closeDiv
+    const addFare = elementFromHtml (
+        // gridInfo Section
+        newGridInfo.idGridIndfo +
+        newGridInfo.idGridInfoLayout +
+        newGridInfo.clSpaceLeft +
+        newGridInfo.clIndex(fareTableCount) +
+        newGridInfo.clExpCon(fareTableCount) +
+        newGridInfo.clTime(fareTableCount) +
+        newGridInfo.clStartAdd(fareTableCount) +
+        newGridInfo.clRunTotal(fareTableCount) +
+        newGridInfo.clSpaceRight +
+        newGridInfo.closeDiv +
+        // gridDetails section
+        newGridDetails.idGridDetails +
+        newGridDetails.idGridDetailsLayout +
+        newGridDetails.clSpaceA1 +
+        newGridDetails.clSpaceA2 +
+        newGridDetails.clSpaceA3 +
+        newGridDetails.clDetailsEndTime(fareTableCount) +
+        newGridDetails.clDetailsDestination(fareTableCount) +
+        newGridDetails.clDetailsFareTotal(fareTableCount) +
+        newGridDetails.clSpaceB1 +
+        newGridDetails.clSpaceB2 +
+        newGridDetails.clSpaceB3 +
+        newGridDetails.clSpaceB4 +
+        newGridDetails.clDetailsTotalTime(fareTableCount) +
+        newGridDetails.clDetailsDistance(fareTableCount) +
+        newGridDetails.clDetailDollarsPerKm(fareTableCount) +
+        newGridDetails.clSpaceC1 +
+        newGridDetails.clSpaceC2 +
+        newGridDetails.clSpaceC3 +
+        newGridDetails.clSpaceC4 +
+        newGridDetails.clDetailsFareType(fareTableCount) +
+        newGridDetails.clDetailsTip(fareTableCount) +
+        newGridDetails.clSpaceC5
+    );
 
-    // gridDetails section
-    newGridDetails.idGridDetails(fareTableCount) +
-    newGridDetails.idGridDetailsLayout(fareTableCount) +
-    newGridDetails.clSpaceA1 +
-    newGridDetails.clSpaceA2 +
-    newGridDetails.clSpaceA3 +
-    newGridDetails.clDetailsEndTime(fareTableCount) +
-    newGridDetails.clDetailsDestination(fareTableCount) +
-    newGridDetails.clDetailsFareTotal(fareTableCount) +
-    newGridDetails.clSpaceB1 +
-    newGridDetails.clSpaceB2 +
-    newGridDetails.clSpaceB3 +
-    newGridDetails.clSpaceB4 +
-    newGridDetails.clDetailsTotalTime(fareTableCount) +
-    newGridDetails.clDetailsDistance(fareTableCount) +
-    newGridDetails.clDetailDollarsPerKm(fareTableCount) +
-    newGridDetails.clSpaceC1 +
-    newGridDetails.clSpaceC2 +
-    newGridDetails.clSpaceC3 +
-    newGridDetails.clSpaceC4 +
-    newGridDetails.clDetailsFareType(fareTableCount) +
-    newGridDetails.clDetailsTip(fareTableCount) +
-    newGridDetails.clSpaceC5
+    fareTableElement.appendChild(addFare);
 }
 
 /* old function 
